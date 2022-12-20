98a19c2d75ba0583c5869a43691b1098afd35703
canhhuy116 <canhhuy1162002@gmail.com>
2022-12-21 01:23:07 +0700

    make a class page

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -5,7 +5,7 @@ function Button({ nameBtn, className, onClickButton }) {
     <button
       type="button"
       onClick={onClickButton}
-      className={`mt-6 flex w-96 items-center justify-center rounded-md border border-transparent bg-indigo-600 py-3 px-8 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 ${className}-btn`}
+      className={`mt-6 flex items-center justify-center rounded-md border border-transparent bg-indigo-600 py-3 px-8 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 ${className}`}
     >
       {nameBtn}
     </button>
--- src/pages/MakeClass.jsx
+++ src/pages/MakeClass.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import Table from '../components/Table';
+import Button from '../components/Button';
 
 /**
  * This page shows the marks of all students with a specific subject.
@@ -10,9 +12,85 @@ export default function MakeClass() {
   const [numOfStudents, setNumOfStudents] = React.useState(0);
   const [students, setStudents] = React.useState([]);
 
+  const data = {
+    column_names: ['No.', 'Name', 'Date', 'Gender', 'Address', 'Email'],
+    rows: [
+      {
+        name: 'Nguyen Van A',
+        results: ['1/1/2000', 'Male', 'VN', 'abc@email.com'],
+      },
+      {
+        name: 'Nguyen Van B',
+        results: ['1/1/2000', 'Male', 'VN', 'abc@email.com'],
+      },
+      {
+        name: 'Nguyen Van C',
+        results: ['1/1/2000', 'Male', 'VN', 'abc@email.com'],
+      },
+      {
+        name: 'Nguyen Van D',
+        results: ['1/1/2000', 'Male', 'VN', 'abc@email.com'],
+      },
+    ],
+  };
+
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      Make Class
+    <div className="flex flex-col items-center justify-center max-w-7xl min-h-screen py-2">
+      <h1 className="text-5xl font-bold my-10">Make a Class</h1>
+      <div className="flex flex-nowrap mb-10">
+        <div className="mr-10">
+          <label htmlFor="name" className="text-base font-bold mr-5">
+            Name
+          </label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            className="border-1 border-black rounded-md p-1"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="numOfStudents" className="text-base font-bold mr-5">
+            Number of Students
+          </label>
+          <input
+            type="number"
+            id="numOfStudents"
+            name="numOfStudents"
+            className="border-1 border-black rounded-md p-1"
+            value={numOfStudents}
+            onChange={(e) => setNumOfStudents(e.target.value)}
+          />
+        </div>
+      </div>
+      <Table data={data} />
+
+      <button className="mt-10">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-12 h-12 text-blue-500 hover:text-blue-700 cursor-pointer"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+          />
+        </svg>
+      </button>
+
+      <div className="flex">
+        <Button
+          nameBtn={'Cancel'}
+          className={'mr-10 bg-neutral-400 hover:bg-gray-700'}
+        />
+        <Button nameBtn={'Done'} />
+      </div>
     </div>
   );
 }
