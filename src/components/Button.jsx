ac5f2c19117f632d2d457c15cd2e1ee8c3ec9d85
canhhuy116 <canhhuy1162002@gmail.com>
2022-12-20 21:26:53 +0700

    add Requirement btn

---
+++ src/components/Button.jsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+
+function Button({ nameBtn, className, onClickButton }) {
+  return (
+    <button
+      type="button"
+      onClick={onClickButton}
+      className={`mt-6 flex w-96 items-center justify-center rounded-md border border-transparent bg-indigo-600 py-3 px-8 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 ${className}-btn`}
+    >
+      {nameBtn}
+    </button>
+  );
+}
+
+export default Button;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Button from '../components/Button';
 
 /**
  * This page shows a list of students' information.
@@ -7,5 +8,44 @@ import React from 'react';
  * @returns JSX.Element as a page
  */
 export default function Home() {
-	return <div>Home</div>;
+  const onClickButton = () => {
+    console.log('Button clicked');
+  };
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
+        <h1 className="text-5xl font-bold">Requirement</h1>
+        <Button
+          nameBtn="Recept Student"
+          className="bg-green-500 hover:bg-green-600"
+          onClickButton={onClickButton}
+        />
+        <Button
+          nameBtn="Make a class list"
+          className="bg-green-500 hover:bg-green-600"
+          onClickButton={onClickButton}
+        />
+        <Button
+          nameBtn="Search Student"
+          className="bg-green-500 hover:bg-green-600"
+          onClickButton={onClickButton}
+        />
+        <Button
+          nameBtn="Get a subject score sheet"
+          className="bg-green-500 hover:bg-green-600"
+          onClickButton={onClickButton}
+        />
+        <Button
+          nameBtn="Make a summary report"
+          className="bg-green-500 hover:bg-green-600"
+          onClickButton={onClickButton}
+        />
+        <Button
+          nameBtn="Change rules"
+          className="bg-green-500 hover:bg-green-600"
+          onClickButton={onClickButton}
+        />
+      </main>
+    </div>
+  );
 }
