633690692ed2b3b6f823baeb589977d859660117
20120474 <84512387+kimhieu153255@users.noreply.github.com>
2022-12-30 19:20:06 +0700

    handle sign-up

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
-import { Routes, Route } from 'react-router-dom';
-import SignIn from './pages/SignIn';
-import SignUp from './pages/SignUp';
-import Dashboard from './pages/Dashboard';
-import Home from './pages/Home';
-import Marks from './pages/Marks';
-import Report from './pages/Report';
-import Profile from './pages/Profile';
-import NotFound from './pages/NotFound';
-import Class from './pages/Class';
-import SearchStudent from './pages/SearchStudent';
-import Student from './pages/Student';
+import { Routes, Route } from "react-router-dom";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Dashboard from "./pages/Dashboard";
+import Home from "./pages/Home";
+import Marks from "./pages/Marks";
+import Report from "./pages/Report";
+import Profile from "./pages/Profile";
+import NotFound from "./pages/NotFound";
+import Class from "./pages/Class";
+import SearchStudent from "./pages/SearchStudent";
+import Student from "./pages/Student";
+import Notify from "./pages/Notify";
 
 function App() {
   return (
@@ -24,6 +25,7 @@ function App() {
           <Route path="student/:id" element={<Student />} />
           <Route path="marks/:id" element={<Marks />} />
           <Route path="report" element={<Report />} />
+          <Route path="notify" element={<Notify />} />
           <Route path="profile" element={<Profile />} />
           <Route path="class/add" element={<Class />} />
           <Route path="class/:id" element={<Class />} />
@@ -33,7 +35,6 @@ function App() {
       </Routes>
     </>
   );
-
 }
 
 export default App;
