c24619b0b495e11e98902e89a4cc76638f76e1d9
PhiHungTF <phihungtf@gmail.com>
2023-01-06 12:44:17 +0700

    add class icon

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,14 +2,15 @@ import React from "react";
 import SidebarIcon from "./SidebarIcon";
 import { Link } from "react-router-dom";
 import {
-  FaSchool,
-  FaHome,
-  FaUserGraduate,
-  FaClipboardCheck,
-  FaClipboardList,
-  FaUser,
-  FaSignOutAlt,
-  FaBell,
+	FaSchool,
+	FaHome,
+	FaElementor,
+	FaUserGraduate,
+	FaClipboardCheck,
+	FaClipboardList,
+	FaUser,
+	FaSignOutAlt,
+	FaBell,
 } from "react-icons/fa";
 
 /**
@@ -17,49 +18,52 @@ import {
  * @returns JSX.Element as a component
  */
 export default function Sidebar() {
-  function handleLogout() {
-    localStorage.removeItem("token");
-    localStorage.removeItem("user");
-    localStorage.removeItem("role");
-  }
-  function handleRole() {
-    let role = parseInt(localStorage.getItem("role"));
-    console.log(role);
-    if (role >= 1) {
-      return (
-        <Link to={`notify`}>
-          <SidebarIcon icon={<FaBell size="30" />} text="Notify" />
-        </Link>
-      );
-    } else {
-      return <></>;
-    }
-  }
-  return (
-    <div className="fixed top-0 left-0 h-screen w-[250px] flex flex-col bg-violet-900">
-      <Link to={``}>
-        <SidebarIcon icon={<FaSchool size="35" />} text="Student Management" />
-      </Link>
-      <hr className="border-violet-500" />
-      <Link to={``}>
-        <SidebarIcon icon={<FaHome size="30" />} text="Home" />
-      </Link>
-      <Link to={`student`}>
-        <SidebarIcon icon={<FaUserGraduate size="30" />} text="Student" />
-      </Link>
-      <Link to={`marks/2`}>
-        <SidebarIcon icon={<FaClipboardCheck size="30" />} text="Marks" />
-      </Link>
-      <Link to={`report`}>
-        <SidebarIcon icon={<FaClipboardList size="30" />} text="Report" />
-      </Link>
-      {handleRole()}
-      <Link to={`profile`} className="mt-auto">
-        <SidebarIcon icon={<FaUser size="30" />} text="Profile" />
-      </Link>
-      <Link onClick={handleLogout} to={`../signin`}>
-        <SidebarIcon icon={<FaSignOutAlt size="30" />} text="Sign Out" />
-      </Link>
-    </div>
-  );
+	function handleLogout() {
+		localStorage.removeItem("token");
+		localStorage.removeItem("user");
+		localStorage.removeItem("role");
+	}
+	function handleRole() {
+		let role = parseInt(localStorage.getItem("role"));
+		console.log(role);
+		if (role >= 1) {
+			return (
+				<Link to={`notify`}>
+					<SidebarIcon icon={<FaBell size="30" />} text="Notify" />
+				</Link>
+			);
+		} else {
+			return <></>;
+		}
+	}
+	return (
+		<div className="fixed top-0 left-0 h-screen w-[250px] flex flex-col bg-violet-900">
+			<Link to={``}>
+				<SidebarIcon icon={<FaSchool size="35" />} text="Student Management" />
+			</Link>
+			<hr className="border-violet-500" />
+			<Link to={``}>
+				<SidebarIcon icon={<FaHome size="30" />} text="Home" />
+			</Link>
+			<Link to={`class/1`}>
+				<SidebarIcon icon={<FaElementor size="30" />} text="Class" />
+			</Link>
+			<Link to={`student`}>
+				<SidebarIcon icon={<FaUserGraduate size="30" />} text="Student" />
+			</Link>
+			<Link to={`marks/2`}>
+				<SidebarIcon icon={<FaClipboardCheck size="30" />} text="Marks" />
+			</Link>
+			<Link to={`report`}>
+				<SidebarIcon icon={<FaClipboardList size="30" />} text="Report" />
+			</Link>
+			{handleRole()}
+			<Link to={`profile`} className="mt-auto">
+				<SidebarIcon icon={<FaUser size="30" />} text="Profile" />
+			</Link>
+			<Link onClick={handleLogout} to={`../signin`}>
+				<SidebarIcon icon={<FaSignOutAlt size="30" />} text="Sign Out" />
+			</Link>
+		</div>
+	);
 }
